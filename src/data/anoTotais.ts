bd865db33afd4c1ae0c83140b8a0e1b3a1335a36
Arthur Felipe <artur-felipe-14@hotmail.com>
2025-11-22 22:05:32 -0300

    [ADD] Anos [ADD] imagens de marcas [EDIT] types atualizados

---
+++ src/data/anoTotais.ts
@@ -0,0 +1,28 @@
+// src/data/anoTotais.ts
+
+export const totaisPorAno: { [key: number]: number } = {
+    1998: 187,
+    2005: 187,
+    2006: 223,
+    2007: 180,
+    2008: 196,
+    2009: 190,
+    2010: 240,
+    2011: 244,
+    2012: 247,
+    2013: 250,
+    2014: 250,
+    2015: 250,
+    2016: 250,
+    2017: 365,
+    2018: 365,
+    2019: 250,
+    2020: 250,
+    2021: 250,
+    2022: 250,
+};
+
+// Se você precisar de um valor padrão para anos não listados, pode adicionar uma função auxiliar
+export function getTotalPorAno(ano: number): number | undefined {
+    return totaisPorAno[ano] || 250;
+}
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,25 @@
 // src/types.ts
 
 export interface Carrinho {
-    id: string;         // identificador único
-    codigo?: string;    // Código do carrinho - Ex: CFH20
-    nome: string;       // LaFerrari
-    brand?: string;     // Hot WHeels, Matchbox, GreenLight, etc
-    marca?: string;     // Ferrari
-    cor?: string;       // Vermelho
-    color?: string;     // Metalflake Red
-    pais?: string;      // Itália
+    id: string;                 // identificador único
+    codigo?: string;            // Código do carrinho - Ex: CFH20
+    nome: string;               // LaFerrari
+    brand?: string;             // Hot Wheels, Matchbox, GreenLight, etc
+    marca?: string;             // Ferrari
+    cor?: string;               // Vermelho
+    color?: string;             // Metalflake Red
+    pais?: string;              // Itália
+    anoLancamento ?: number;    // Ano que foi lançado
+    serieName?: string;         // Nome da série (Fast & Furious, etc)
+    serieTipo?: string;         // Tipo da série (pack 5, mainline, premium, etc)
+    numeroSerie?:number;        // Número do carrinho na série (Ex: 2)
+    totalSerie?:number;         // Número total da série (Ex: 5)
+    numeroAno?: number;         // Número da mini naquele ano (Ex: 120/250)
 
-    rubber?: boolean;   // Pneu de borracha?
+    treasureHunt?: string;      // R=RTH; S=STH; N=Mainline; 
+    newModel?: boolean;         // É o 1º modelo?
+    rubber?: boolean;           // Pneu de borracha?
+    custom?: boolean;           // É custom?
+
+    urlImage?: string;          // URL da imagem do carrinho
 }
